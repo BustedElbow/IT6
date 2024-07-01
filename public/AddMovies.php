93c94bc7a36f8871f2c165829bc41f433b513a1c
Miguel <maevenr81@gmail.com>
2024-07-01 23:18:58 +0800

    AddMovies Partial

--- public/AddMovies.php
+++ public/AddMovies.php
@@ -5,73 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Movies</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="app.css">
 </head>
-<div>
-
-    <body>
-        <div>
-            <div>
-                <h1 class="font-bold uppercase p-4 border">
-                    <a href="index.html">Movie Manager</a>
-                </h1>
-            </div>
-
-        </div> <!-- sidebar-->
-
-        <ul class="text-white fixed font-semibold p-2 w-36 h-screen bg-gray-800 divide-y">
-            <li class="">
-                <a href="#">
-                    <span>Search</span>
-                </a>
-            </li>
-            <li class=>
-                <a href="#">
-                    <span>Home</span>
-                </a>
-            </li>
-            <li class="">
-                <a href="#">
-                    <span>Your Movies</span>
-                </a>
-            </li>
-            <li class="">
-                <a href="AddMovies.html">
-                    <span>Add Movies</span>
-                </a>
-            </li>
-        </ul>
-        </nav>
-
-
-        <div class="flex justify-center items-start h-screen pl-12rem">
-            <form>
-                <label for="title">Title:</label>
-                <input type="text" id="title" name="title">
-                <label for="director">Director:</label>
-                <input type="text" id="director" name="director">
-                <label for="year"
-                    class="bg-pink-500 hover:bg-blue-700 text-white font-bold py-0.5 px-4 rounded">Year:</label>
-                <input type="text" id="year" name="year" class="border-blue-500">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><a
-                        href="index.html">Add
-                        Movie</a></button>
-            </form>
-        </div>
-    </body>
-</div>
-
-
-
-<body class="flex">
-    <nav>
-        <ul class="text-white font-semibold p-2 w-36 h-screen bg-gray-800 divide-y">
-            <span class="">Movie Manager</span>
-            <li class="">
-                <a href="#">
-                    <span>Search</span>
-                </a>
-            </li>
+<body>
+    <div>
+        <nav>
+        <ul class= sidebaraddmovies>
             <li class=>
                 <a href="index.html">
                     <span>Home</span>
@@ -89,33 +28,42 @@
             </li>
         </ul>
     </nav>
+    </div>
     <main>
-        <table class="table-auto">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Year</th>
-                </tr>
-            <tbody>
-                <tr>
-                    <td>The Lorax</td>
-                    <td>Dr. Seuss</td>
-                    <td>1961</td>
-                </tr>
-                <tr>
-                    <td>Witch Woman</td>
-                    <td>The Eagles</td>
-                    <td>1972</td>
-                </tr>
-                <tr>
-                    <td>Shining Star</td>
-                    <td>Earth, Wind, and Fire</td>
-                    <td>1975</td>
-                </tr>
-            </tbody>
-            </thead>
-        </table>
+        <div class="tablebody">
+            <div class="mov">
+                <h1>MOVIES</h1>
+                <button>Add</button>
+            </div>
+            <div class="tablecontainer">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Release</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>The Lorax</td>
+                            <td>2012</td>
+                            <td>4/5</td>
+                        </tr>
+                        <tr>
+                            <td>Sausage Party</td>
+                            <td>2016</td>
+                            <td>3.5/5</td>
+                        </tr>
+                        <tr>
+                            <td>Equalizer</td>
+                            <td>2014</td>
+                            <td>4.5/5</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 </body>
 
