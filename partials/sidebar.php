a0bc2db4ee08afd7d679b460e3230f0cd7a6df17
bustedelbow <velozity2@gmail.com>
2024-07-02 00:51:38 +0800

    Updated the anchor href

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -2,8 +2,8 @@
   <div class="sidebar-div">
     <span>Menu</span>
     <ul>
-      <li><a id="homeLink" href="../public/index.php">Home</a></li>
-      <li><a id="moviesLink" href="../public/movielist.php">Movie List</a></li>
+      <li><a id="homeLink" href="../it6/index.php">Home</a></li>
+      <li><a id="moviesLink" href="../it6/movielist.php">Movie List</a></li>
     </ul>
   </div>
   <div class="sidebar-div">
