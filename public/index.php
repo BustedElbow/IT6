fc3b0d4a831e4b41acc69149b35553f71150c310
bustedelbow <velozity2@gmail.com>
2024-07-01 21:53:55 +0800

    Removed tailwind

--- public/index.php
+++ public/index.php
@@ -5,19 +5,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tailwind Test</title>
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="AddMovies.html">
 </head>
 
 <body class="flex">
 
   <?php require('partials/sidebar.php'); ?>
 
-  <div>
-    <main>
-      <a href="#">Log In</a>
-      <a href="#">Sign Up</a>
-      <!--<button class="border-l-pink-50">Add Movies</button>
+  
+  <main class="bg-primary border-lime-50 ">
+    <a href="#">Log In</a>
+    <a href="#">Sign Up</a>
+    
 
   </main>
 </body>
--- public/partials/sidebar.php
+++ public/partials/sidebar.php
@@ -1,25 +1,34 @@
-<nav>
-    <ul class="text-white font-semibold p-2 w-36 h-screen bg-gray-800 divide-y">
-      <span class="">Movie Manager</span>
-      <li class="">
-        <a href="#">
-          <span>Search</span>
-        </a>
-      </li>
-      <li class=>
-        <a href="index.html">
-          <span>Home</span>
-        </a>
-      </li>
-      <li class="">
-        <a href="#">
-          <span>Your Movies</span>
-        </a>
-      </li>
-      <li class="">
-        <a href="AddMovies.html">
-          <span>Add Movies</span>
-        </a>
-      </li>
-    </ul>
-  </nav>
+<!-- <nav>
+        <ul class="text-white font-semibold p-2 w-64 h-screen bg-gray-800 divide-y">
+            <span class="">Movie Manager</span>
+            <li class="">
+                <a href="#">
+                    <span>Search</span>
+                </a>
+            </li>
+            <li class=>
+                <a href="index.html">
+                    <span>Home</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="#">
+                    <span>Your Movies</span>
+                </a>
+            </li>
+            <li class="">
+                <a href="AddMovies.html">
+                    <span>Add Movies</span>
+                </a>
+            </li>
+        </ul>
+    </nav> -->
+<nav class="sidebar">
+  <span>Menu</span>
+  <ul>
+    <li>Home</li>
+    <li>Movie List</li>
+  </ul>
+  
+
+</nav>
