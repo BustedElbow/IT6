637b6587c47d222aa0dd359c5960303a600fceb4
bustedelbow <velozity2@gmail.com>
2024-07-02 10:03:53 +0800

    Formatted properly

--- movielist.php
+++ movielist.php
@@ -4,49 +4,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Movies</title>
+    <title>Movies List</title>
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
   
-    <?php require('partials/sidebar.php'); ?>
+  <?php require('partials/sidebar.php'); ?>
 
-    <main>
-        <div class="tablebody">
-            <div class="mov">
-                <h1>MOVIES</h1>
-                <button>Add</button>
-            </div>
-            <div class="tablecontainer">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Release</th>
-                            <th>Rating</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>The Lorax</td>
-                            <td>2012</td>
-                            <td>4/5</td>
-                        </tr>
-                        <tr>
-                            <td>Sausage Party</td>
-                            <td>2016</td>
-                            <td>3.5/5</td>
-                        </tr>
-                        <tr>
-                            <td>Equalizer</td>
-                            <td>2014</td>
-                            <td>4.5/5</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </main>
+  <main>
+    <div class="tablebody">
+      <div class="mov">
+        <h1>MOVIES</h1>
+        <button>Add</button>
+      </div>
+      <div class="tablecontainer">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Release</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Lorax</td>
+              <td>2012</td>
+              <td>4/5</td>
+            </tr>
+            <tr>
+              <td>Sausage Party</td>
+              <td>2016</td>
+              <td>3.5/5</td>
+            </tr>
+            <tr>
+              <td>Equalizer</td>
+              <td>2014</td>
+              <td>4.5/5</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </main>
   <script src="script.js"></script>
 </body>
 
