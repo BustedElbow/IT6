ba1f4e684932aca1183dabce14bf007ae5be86c8
bustedelbow <velozity2@gmail.com>
2024-07-02 22:16:32 +0800

    Added test form

--- index.php
+++ index.php
@@ -12,11 +12,18 @@
 <body>
 
   <?php require('partials/sidebar.php'); ?>
-
   
   <main class="main">
-    <a href="#">Log In</a>
-    <a href="#">Sign Up</a>
+    <h2>Add Movie</h2>
+    <form action="addmovie.php" method="post">
+      <label for="title">Title:</label><br>
+      <input type="text" id="title" name="title" required><br>
+      <label for="director">Director:</label><br>
+      <input type="text" id="director" name="director" required><br>
+      <label for="year">Year:</label><br>
+      <input type="date" id="year" name="year" required><br>
+      <input type="submit" value="Submit">
+    </form>
   </main>
   <script src="script.js"></script>
 </body>
