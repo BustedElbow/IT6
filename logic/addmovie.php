0add6d38934e958cda4cc9837ffdcae62795466d
bustedelbow <velozity2@gmail.com>
2024-07-04 20:36:17 +0800

    Update file path

--- logic/addmovie.php
+++ logic/addmovie.php
@@ -2,22 +2,20 @@
 include 'connection.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect value of input field
+
     $title = mysqli_real_escape_string($conn, $_POST['movieTitle']);
     $director = mysqli_real_escape_string($conn, $_POST['director']);
     $releaseYear = mysqli_real_escape_string($conn, $_POST['releaseYear']);
 
-    // SQL query to insert data into the movies table
     $sql = "INSERT INTO tbl_movie (title, director, releaseYear) VALUES ('$title', '$director', '$releaseYear')";
 
-    // Execute the query
     if (mysqli_query($conn, $sql)) {
         echo "New record created successfully";
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 }
-header('Location: movielist.php');
-// Close the connection
+header('Location: ../movielist.php');
+
 mysqli_close($conn);
 ?>
