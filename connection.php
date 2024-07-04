478b7e7dec609b0ea29f4dd9ea2d29ab0e8dc0f3
bustedelbow <velozity2@gmail.com>
2024-07-04 10:48:18 +0800

    Reverted from PDO to MySQLI

--- connection.php
+++ connection.php
@@ -1,20 +1,12 @@
 <?php
-$host = 'localhost';
-$db   = 'db_movies';
-$user = 'root';
-$pass = 'admin123';
-$charset = 'utf8mb4';
+$servername = "localhost";
+$username = "root";
+$password = "admin123";
+$database = "db_movies";
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 ?>
