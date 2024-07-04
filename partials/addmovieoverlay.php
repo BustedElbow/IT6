7d98fa7a4bb349fa237208aafe8145900e96caf7
bustedelbow <velozity2@gmail.com>
2024-07-04 12:41:19 +0800

    Created logic folder

--- partials/addmovieoverlay.php
+++ partials/addmovieoverlay.php
@@ -1,10 +1,10 @@
-<div id="overlay" style="display:none;">
+<div id="addmovieOverlay" style="display:none;">
   <div class="overlay-content">
     <span class="close" onclick="hideOverlay()">&times;</span>
     <form id="addMovieForm" action="addmovie.php" method="POST">
       <input type="text" name="movieTitle" placeholder="Movie Title" required>
-      <input type="date" name="director" placeholder="Director" required>
-      <input type="text" name="releaseYear" placeholder="Release Year" required>
+      <input type="text" name="director" placeholder="Director" required>
+      <input type="date" name="releaseYear" placeholder="Release Year" required>
       <button type="submit">Submit</button>
     </form>
   </div>
