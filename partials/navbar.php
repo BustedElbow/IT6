d48e94df431bf450a838adad674b3d64eb4d7c4c
bustedelbow <velozity2@gmail.com>
2024-07-12 21:19:22 +0800

    Minor UI changes

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,28 +2,31 @@
 session_start()
 ?>
 
-<nav class="sticky-top">
+<nav class="sticky-top nav-bar-custom">
+  
   <div class="bg-primary d-lg-none">
     <button class="btn d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
       <i class="lni lni-menu"></i>
     </button>
   </div>
   
-  <div class="d-none d-lg-block bg-primary">
-    <ul class="nav justify-content-center">
-      <li class="nav-item">
-      <?php if(isset($_SESSION['username'])): ?>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Hello, <?= htmlspecialchars($_SESSION['username']); ?>
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="../src/login.view.php">Logout</a></li>
-          </ul>
-        </li>
-      <?php endif; ?>
-      </li>
-    </ul>
+  <div class="container">
+    <div class="d-none d-lg-block ms-auto">
+      <ul class="nav justify-content-between">
+        <a class="nav-brand" href="#">Brand</a>
+          <?php if(isset($_SESSION['username'])): ?>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Hello, <?= htmlspecialchars($_SESSION['username']); ?>
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="../src/login.view.php">Logout</a></li>
+              </ul>
+            </li>
+          <?php endif; ?>
+        </ul>
+      </div>
+    </div>
   </div>
   
   <div class="offcanvas offcanvas-start d-lg-none" tabindex="-1" id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel" >
