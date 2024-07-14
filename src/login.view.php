fee15f553aa998329cc2777f4b94dfc570c44879
bustedelbow <velozity2@gmail.com>
2024-07-14 11:44:14 +0800

    Updated UI

--- src/login.view.php
+++ src/login.view.php
@@ -8,31 +8,30 @@
   <link rel="stylesheet" href="main.css">
 </head>
 <body>
-  <div class="d-flex justify-content-center align-items-center vh-90">
-    <div class="container p-5">
-      <div class="row custom-bg-color border border-radius-container">
+  <div class="d-flex justify-content-center align-items-center min-vh-100 ">
+      <div class="login-container">
         <div class="col-md-12">
           <div class="text-center">
-            <img src="../src/images/Logo/FilmShelf.png" alt="Logo" class="img-fluid">
+            <img src="images/logo/logo.png" alt="Logo" class="">
           </div>
         </div>
-        <div class="col-md-4 custom-form-height mx-auto">
-          <h2 class="align-self-center text-center">Login</h2>
+        <div class="">
             <form action="../logic/login.php" method="POST">
               <div class="form-group">
-                <label for="email">Username:</label>
+                <label for="email">Username</label>
                 <input type="text" class="form-control" id="email" name="username" required>
               </div>
               <div class="form-group mb-3">
-                <label for="password">Password:</label>
+                <label for="password">Password</label>
                 <input type="password" class="form-control" id="password" name="password" required>
               </div>
-              <button type="submit" name="login" class="btn btn-primary">Login</button>
+              <button type="submit" name="login" class="btn custom-btn w-100">Login</button>
             </form>
-            <span>Don't have an account? <a href="register.view.php" class="align-self-center">Signup</a></span>
+            <div class="d-flex justify-content-center mt-4">
+              <span>Don't have an account? <a href="register.view.php" class="align-self-center">Signup</a></span>
+            </div>
         </div>
       </div>
-    </div>
   </div>
   <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
