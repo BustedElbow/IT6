d5a278b46f27fb27ef87bf6129bb0849256f8b17
bustedelbow <velozity2@gmail.com>
2024-07-17 01:07:17 +0800

    Moved kebab position

--- partials/movielist.php
+++ partials/movielist.php
@@ -33,7 +33,7 @@ if (!empty($search)) {
 <div class="container">
   <div class="row justify-content-between align-items-center mt-3 mb-2">
     <div class="col-auto">
-      <h2>Your Movies</h2>
+      <h2>Movies</h2>
     </div>
     <div class="col-auto">
       <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#addMovieModal">
@@ -45,20 +45,14 @@ if (!empty($search)) {
     <?php
     $hasMovies = false; 
     foreach ($movies as $movie):
-      if ($_SESSION['userID'] == ($movie['userID'] ?? null)):
         $hasMovies = true;
         ?>
         <div class="col-lg-3 col-md-4 mb-3">
-          <div class="card card-color w-auto h-100">
+          <div class="card card-color w-auto h-100 position-relative">
             <img src="<?= !empty($movie['thumbnail']) ? '../src/images/movies/' . htmlspecialchars($movie['thumbnail']) : '../src/images/movies/no_image.png'; ?>" class="card-img-top img-fluid" alt="<?= htmlspecialchars($movie['title'] ?? 'Default Title'); ?>">
-            <div class="card-body">
-              <h5 class="card-title"><?= htmlspecialchars($movie['title'] ?? ''); ?></h5>
-              <p class="card-text mb-1">Directed by <?= htmlspecialchars($movie['director'] ?? ''); ?></p>
-              <p class="card-text mb-2">Released on <?= !empty($movie['releaseYear']) ? date_format(date_create($movie['releaseYear']), 'F j, Y') : ''; ?></p>
-              <p class="card-text mb-3"><span class="genre-tag"><?= htmlspecialchars($movie['genre'] ?? '--Not Set--'); ?></span></p> 
-              <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton<?php echo $movie['movieID']; ?>" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-three-dots-vertical"></i> <!-- Bootstrap Icons for kebab menu -->
+            <div class="dropdown position-absolute top-0 end-0">
+                <button class="btn btn-secondary" type="button" id="dropdownMenuButton<?php echo $movie['movieID']; ?>" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="lni lni-more-alt"></i>
                 </button>
           
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton<?php echo $movie['movieID']; ?>">
@@ -76,15 +70,23 @@ if (!empty($search)) {
                     <li><a class="dropdown-item" href="#">Watch Later</a></li>
                 </ul>
               </div>
-              <p class="card-text mb-3 comment fst-italic" data-bs-toggle="modal" data-bs-target="#fullCommentModal" data-full-comment="<?= htmlspecialchars($movie['comment'] ?? '--Not Set--'); ?>"><span class="comment-tag"><?= htmlspecialchars($movie['comment'] ?? '--Not Set--',15);?></span></p>
+            <div class="card-body">
+              <h5 class="card-title"><?= htmlspecialchars($movie['title'] ?? ''); ?></h5>
+              <p class="card-text mb-1">Directed by <?= htmlspecialchars($movie['director'] ?? ''); ?></p>
+              <p class="card-text mb-2">Released on <?= !empty($movie['releaseYear']) ? date_format(date_create($movie['releaseYear']), 'F j, Y') : ''; ?></p>
+              <p class="card-text mb-3"><span class="genre-tag"><?= htmlspecialchars($movie['genre'] ?? '--Not Set--'); ?></span></p> 
               
+              <p class="card-text mb-3 comment fst-italic" data-bs-toggle="modal" data-bs-target="#fullCommentModal" data-full-comment="<?= htmlspecialchars($movie['comment'] ?? '--Not Set--'); ?>"><span class="comment-tag"><?= htmlspecialchars($movie['comment'] ?? '--Not Set--',15);?></span></p>
+              <div class="d-flex gap-0 mt-4">
+                <img src="../src/images/users/<?= !empty($movie['picture']) ? htmlspecialchars($movie['picture']) : 'no_image.png'; ?>" class="img-circle-sm" alt="User Picture">
+                <span class="align-self-center"><?= htmlspecialchars($movie['username'] ?? ''); ?></span>
+              </div>
                 
                 
               
             </div>
           </div>
         </div>
-      <?php endif; ?>
     <?php endforeach; ?>
     <?php if (!$hasMovies): ?>
       <div class="col-12 text-center">
@@ -93,58 +95,6 @@ if (!empty($search)) {
     <?php endif; ?>
   </div>
 
-  <h2 class="mb-3">Other Movies</h2>
-  <div class="row gx-3">
-    <?php
-    $otherMovies = false; 
-    foreach ($movies as $movie):
-      if ($_SESSION['userID'] != ($movie['userID'] ?? null)):
-        $otherMovies = true; ?>
-        <div class="col-lg-3 col-md-4 mb-3 h-100">
-          <div class="card card-color w-auto">
-          <img src="<?= !empty($movie['thumbnail']) ? '../src/images/movies/' . htmlspecialchars($movie['thumbnail']) : '../src/images/movies/no_image.png'; ?>" class="card-img-top img-fluid" alt="<?= htmlspecialchars($movie['title'] ?? 'Default Title'); ?>">           
-            <div class="card-body">
-              <h5 class="card-title"><?= htmlspecialchars($movie['title'] ?? ''); ?></h5>
-              <p class="card-text mb-1">Directed by <?= htmlspecialchars($movie['director'] ?? ''); ?></p>
-              <p class="card-text mb-2">Released on <?= !empty($movie['releaseYear']) ? date_format(date_create($movie['releaseYear']), 'F j, Y') : ''; ?></p>
-              <p class="card-text mb-1"><span class="genre-tag"><?= htmlspecialchars($movie['genre'] ?? '--Not Set--'); ?></span></p>
-              <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton<?php echo $movie['movieID']; ?>" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="bi bi-three-dots-vertical"></i> <!-- Bootstrap Icons for kebab menu -->
-                </button>
-          
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton<?php echo $movie['movieID']; ?>">
-                  <?php if ($isAdmin || $_SESSION['userID'] == ($movie['userID'] ?? null)): ?>
-                    <li><button type="button" class="btn custom-btn editBtn" data-bs-toggle="modal" data-bs-target="#editMovieModal" 
-                      data-movie-id="<?= htmlspecialchars($movie['movieID'] ?? ''); ?>" 
-                      data-movie-title="<?= htmlspecialchars($movie['title'] ?? ''); ?>" 
-                      data-movie-director="<?= htmlspecialchars($movie['director'] ?? ''); ?>" 
-                      data-release-date="<?= htmlspecialchars($movie['releaseYear'] ?? ''); ?>"
-                      data-movie-genre="<?= htmlspecialchars($movie['genre'] ?? ''); ?>"
-                      data-movie-comment="<?= htmlspecialchars($movie['comment'] ?? ''); ?>">Edit</button></li>
-                    <li><button type="button" class="btn btn-danger deleteBtn" data-bs-toggle="modal" data-bs-target="#confirmDeleteModal" 
-                      data-movie-id="<?= htmlspecialchars($movie['movieID'] ?? ''); ?>">Delete</button></li>
-                  <?php endif; ?>
-                    <li><a class="dropdown-item" href="#">Watch Later</a></li>
-                </ul>
-              </div> 
-              <p class="card-text mb-3 comment fst-italic" data-bs-toggle="modal" data-bs-target="#fullCommentModal" data-full-comment="<?= htmlspecialchars($movie['comment'] ?? '--Not Set--'); ?>"><span class="comment-tag"><?= htmlspecialchars($movie['comment'] ?? '--Not Set--',15);?></span></p>
-              <div class="d-flex gap-0 mt-4">
-                <img src="../src/images/users/<?= !empty($movie['picture']) ? htmlspecialchars($movie['picture']) : 'no_image.png'; ?>" class="img-circle-sm" alt="User Picture">
-                <span class="align-self-center"><?= htmlspecialchars($movie['username'] ?? ''); ?></span>
-              </div>
-            </div>
-          </div>
-        </div>
-      <?php endif; ?>
-    <?php endforeach; ?>
-    <?php if (!$otherMovies): ?>
-      <div class="col-12 text-center">
-        <div class='text-center mt-5'><h4 class='text-secondary'>No movies found</h4></div>
-      </div>
-    <?php endif; ?>
-  </div>
-</div>
 
 <script>
   document.addEventListener('DOMContentLoaded', function() {
